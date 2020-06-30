96b380aa942edba0dea4d6262b45eef811acfa53
wdx <1213871656@qq.com>
2020-06-30 19:47:02 +0800

    插件配置完成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import { Button } from 'vant'
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
+Vue.use(Button)
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
